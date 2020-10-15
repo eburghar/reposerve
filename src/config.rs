4135d0c3a70d5e94b4a2ee7ddb3eb2f063531cbd
Éric BURGHARD <eric.burghard@solarsis.ao>
2020-10-15 13:03:22 +0100

    add file upload

---
+++ src/config.rs
@@ -0,0 +1,21 @@
+use serde::Deserialize;
+use std::collections::BTreeMap;
+use std::fs::File;
+use anyhow::{Result, Context};
+
+#[derive(Deserialize)]
+pub struct Config {
+    pub token: String,
+    pub webhooks: BTreeMap<String, String>,
+}
+
+impl Config {
+    pub fn read(config: &str) -> Result<Config> {
+        // open configuration file
+        let file = File::open(&config).with_context(|| format!("Can't open {}", &config))?;
+        // deserialize configuration
+        let config: Config =
+            serde_yaml::from_reader(file).with_context(|| format!("Can't read {}", &config))?;
+        Ok(config)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,55 @@
+mod args;
+mod config;
+
+use crate::{
+	args::Opts,
+	config::Config
+};
 use actix_files as fs;
-use actix_web::{App, HttpServer};
+use actix_web::{App, HttpServer, HttpResponse, Error, web};
+use anyhow::Result;
+use std::io::Write;
+use actix_multipart::Multipart;
+use futures::{StreamExt, TryStreamExt};
+
+async fn save_file(mut payload: Multipart) -> Result<HttpResponse, Error> {
+	// iterate over multipart stream
+	while let Ok(Some(mut field)) = payload.try_next().await {
+		let content_type = field.content_disposition().unwrap();
+		let filename = content_type.get_filename().unwrap();
+		let filepath = format!("./tmp/{}", sanitize_filename::sanitize(&filename));
+
+		// File::create is blocking operation, use threadpool
+		let mut f = web::block(|| std::fs::File::create(filepath))
+			.await
+			.unwrap();
+
+		// Field in turn is stream of *Bytes* object
+		while let Some(chunk) = field.next().await {
+			let data = chunk.unwrap();
+			// filesystem operations are blocking, we have to use threadpool
+			f = web::block(move || f.write_all(&data).map(|_| f)).await?;
+		}
+	}
+	Ok(HttpResponse::Ok().into())
+}
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new().service(fs::Files::new("/static", ".").show_files_listing())
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+async fn serve() -> std::io::Result<()> {
+	HttpServer::new(|| {
+		App::new()
+			.route("/upload", web::post().to(save_file))
+			.service(fs::Files::new("/", ".").show_files_listing())
+	})
+	.bind("127.0.0.1:8080")?
+	.run()
+	.await
+}
+
+fn main() -> Result<()> {
+	let opts: Opts = argh::from_env();
+	// read yaml config
+	let config = Config::read(&opts.config)?;
+	serve().unwrap();
+	Ok(())
 }
