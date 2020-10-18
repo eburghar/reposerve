78b7f04fa6c6a4bef49d89cb812a69c2a4eee7de
Éric BURGHARD <eric.burghard@solarsis.ao>
2020-10-18 23:46:30 +0100

    add params to upload post to identify version, repo and arch

--- src/main.rs
+++ src/main.rs
@@ -25,27 +25,79 @@ use futures::{
 };
 use std::io::Write;
 use std::process::Command;
+use std::fs::{self, File};
 use log::info;
+use sanitize_filename::sanitize;
+use bytes::{BytesMut, BufMut};
+
+#[derive(Debug)]
+struct ApkInfo {
+	version: String,
+	repo: String,
+	arch: String
+}
+
+impl ApkInfo {
+	pub fn new() -> Self {
+		ApkInfo {version: "edge".to_string(), repo: "main".to_string(), arch: "x86_64".to_string()}
+	}
+
+	pub fn set(&mut self, f: &str, v: String) {
+		match f {
+			"version" => self.version = v,
+			"repo" => self.repo = v,
+			"arch" => self.arch = v,
+			_ => ()
+		}
+	}
+}
 
 #[post("/upload", wrap="TokenAuth")]
 async fn save_file(mut payload: Multipart) -> Result<HttpResponse, Error> {
 	// iterate over multipart stream
+	let mut info = ApkInfo::new();
+	let mut files = Vec::new();
 	while let Ok(Some(mut field)) = payload.try_next().await {
-		let content_type = field.content_disposition().unwrap();
-		let filename = content_type.get_filename().unwrap();
-		let filepath = format!("./tmp/{}", sanitize_filename::sanitize(&filename));
+		if let Some(content_type) = field.content_disposition() {
+			match content_type.get_name() {
+				// save files to tmp dir
+				Some("file") => {
+					if let Some(filename) = content_type.get_filename() {
+						let sane_file = sanitize(&filename);
+						let filepath = format!("./{}", sane_file);
+						files.push(sane_file);
 
-		// File::create is blocking operation, use threadpool
-		let mut f = web::block(|| std::fs::File::create(filepath))
-			.await
-			.unwrap();
+						// File::create is blocking operation, use threadpool
+						let mut f = web::block(|| File::create(filepath))
+							.await
+							.unwrap();
 
-		// Field in turn is stream of *Bytes* object
-		while let Some(chunk) = field.next().await {
-			let data = chunk.unwrap();
-			// filesystem operations are blocking, we have to use threadpool
-			f = web::block(move || f.write_all(&data).map(|_| f)).await?;
+						// Field in turn is stream of *Bytes* objectr
+						while let Some(chunk) = field.next().await {
+							let data = chunk.unwrap();
+							// filesystem operations are blocking, we have to use threadpool
+							f = web::block(move || f.write_all(&data).map(|_| f)).await?;
+						}
+					}
+				},
+				// get other parameters for moving the files
+				Some(f) if f == "version" || f == "repo" || f == "arch" => {
+					let mut data = BytesMut::with_capacity(32);
+					// Field in turn is stream of *Bytes* objectr
+					while let Some(chunk) = field.next().await {
+						data.put(chunk.unwrap());
+					}
+					info.set(f, std::str::from_utf8(&data).unwrap().to_string());
+				},
+				_ => ()
+			}
 		}
+	}
+	// move files to correct destination when we have all the info
+	for file in files {
+		let dir = format!("./{version}/{repo}/{arch}", version=sanitize(&info.version), repo=sanitize(&info.repo), arch=sanitize(&info.arch));
+		fs::create_dir_all(&dir)?;
+		fs::rename(format!{"./{}", file}, format!{"{}/{}", dir, file})?;
 	}
 	Ok(HttpResponse::Ok().into())
 }
